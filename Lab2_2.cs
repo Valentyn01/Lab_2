6bbc1454f1e455c5198b524f6d56917db6df6cae
Valentyn01 <65087151+Valentyn01@users.noreply.github.com>
2021-06-07 18:50:29 +0300

    Add files via upload

---
+++ Lab2_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, x = 0, b = Math.PI, dx = Math.PI / 20;
+            Console.WriteLine("  X\t\t Y");
+            while (x <= b)
+            {
+                if (x < Math.PI / 20)
+                {
+                    Console.WriteLine("|" + Math.Round(x, 2));
+                }
+                else
+                {
+                    y = (Math.Pow(x, 1 / 4) * Math.Tan(x) + 13);
+                    Console.WriteLine("|" + Math.Round(x, 2) + "\t\t" + Math.Round(y, 2));
+
+                }
+                x += dx;
+            }
+        }
+    }
+}
---
+++ Lab_2_1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab_2._1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a;
+            Console.WriteLine("Введiть  номер місяця :");
+            a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Січень");
+                    break;
+                case 2:
+                    Console.WriteLine("Лютий");
+                    break;
+                case 3:
+                    Console.WriteLine("Березень");
+                    break;
+                case 4:
+                    Console.WriteLine("Квітень");
+                    break;
+                case 5:
+                    Console.WriteLine("Травень");
+                    break;
+                case 6:
+                    Console.WriteLine("Червень");
+                    break;
+                case 7:
+                    Console.WriteLine("Липень");
+                    break;
+                case 8:
+                    Console.WriteLine("Серпень");
+                    break;
+                case 9:
+                    Console.WriteLine("Вересень");
+                    break;
+                case 10:
+                    Console.WriteLine("Жовтень");
+                    break;
+                case 11:
+                    Console.WriteLine("Листопад");
+                    break;
+                case 12:
+                    Console.WriteLine("Грудень");
+                    break;
+                default: Console.WriteLine("Такого місяця  немає\n"); break;
+
+            }
+        }
+
+    }
+}
